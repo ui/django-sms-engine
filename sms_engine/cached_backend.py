efdce09ba0d3b2e16635289bf7dfbe60eab4cce7
marsha97 <angelamdm97@gmail.com>
2018-12-04 12:09:17 +0700

    cached_backend module

---
+++ sms_engine/cached_backend.py
@@ -0,0 +1,42 @@
+from django.conf import settings
+from django.core.cache import cache as dj_cache
+from sms_engine.models import Backend
+
+
+if hasattr(settings, 'TEST') and settings.TEST:
+    CACHE_KEY = 'test:backend'
+else:
+    CACHE_KEY = 'backend'
+
+
+def delete():
+    key = CACHE_KEY
+    dj_cache.delete(key)
+
+
+def get(cache=True):
+    """ Get backend aliases as a dictionary which are cached by defaults.
+    """
+    key = CACHE_KEY
+    #  Type Dict[int, List[str]] 
+    backend_dict = {}
+
+    if cache:
+        # Hit cache first
+        backend_aliases = dj_cache.get(key)
+        if backend_aliases:
+            return backend_aliases
+
+    # Cache miss, build the dict
+    backend_dict = {
+        Backend.PRIORITY.high: [],
+        Backend.PRIORITY.normal: [],
+        Backend.PRIORITY.low: [],
+    }
+    backends = Backend.objects.filter(is_active=True).order_by('priority', 'id')
+    for backend in backends:
+        backend_dict[backend.priority].append(backend.alias)
+
+    if cache:
+        dj_cache.set(key, backend_dict)
+    return backend_dict
