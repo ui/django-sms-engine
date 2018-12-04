53ce8e3d3688cd3e44043864270869cae9f6dba4
Steven <stvn.bu@gmail.com>
2018-12-04 09:23:11 +0700

    Dynamic backend with kwargs (#18)
    * Dynamic backend with kwargs
    
    * Delete print
    
    * Update test_backends.py
    
    * Refactor
    
    * Fix test
    
    * refactor
    
    * Refactor

--- sms_engine/backends.py
+++ sms_engine/backends.py
@@ -1,11 +1,3 @@
-import xml.etree.ElementTree as ET
-from xml.etree.ElementTree import ParseError
-
-from django.conf import settings
-
-import requests
-from twilio.rest import TwilioRestClient
-
 from .exceptions import SendSMSError
 
 
@@ -16,8 +8,12 @@ class BaseSMSBackend:
 
     """
 
+    def __init__(self, *args, **kwargs):
+        self.kwargs = kwargs
+
     def send_message(self, sms):
-        raise NotImplementedError('subclasses of BaseSMSBackend must override send_message() method')
+        raise NotImplementedError('subclasses of BaseSMSBackend must '
+                                  'override send_message() method')
 
 
 class DummyBackend(BaseSMSBackend):
@@ -32,36 +28,3 @@ class RaiseExceptionBackend(BaseSMSBackend):
     '''
     def send_message(self, sms):
         raise SendSMSError('SMS sending error')
-
-
-class TwilioBackend(BaseSMSBackend):
-
-    def send_message(self, sms):
-        client = TwilioRestClient(settings.TWILIO_ACCOUNT_NUMBER,
-                                  settings.TWILIO_AUTH_TOKEN)
-
-        response = client.api.account.messages.create(from_=settings.TWILIO_FROM_NUMBER,
-                                                      to=sms.to,
-                                                      body=sms.message)
-        return response.sid
-
-
-class NadyneBackend(BaseSMSBackend):
-
-    def send_message(self, sms):
-        params = {
-            'user': settings.NADYNE_USER,
-            'pwd': settings.NADYNE_PASSWORD,
-            'sender': settings.NADYNE_SENDER,
-            'msisdn': sms.to,
-            'message': sms.message,
-            'description': sms.description
-        }
-        response = requests.get("http://apitoken.nadyne.com/sms.php", params=params)
-        if response.status_code == 200:
-            status = ET.fromstring(response.content).find('Status').text
-            if status != "SENT":
-                raise SendSMSError(status)
-            else:
-                return ET.fromstring(response.content).find('TrxID').text
-        raise SendSMSError("Network Error")
--- sms_engine/settings.py
+++ sms_engine/settings.py
@@ -1,20 +1,37 @@
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
+from .compat import import_attribute, string_types
+
 
 def get_backend(alias=None):
-    return get_available_backends()[alias or 'default']
+    """
+    Return SMS Engine's backend instance
+    """
+    backend_config = get_available_backends()[alias or 'default']
+
+    if isinstance(backend_config, string_types):
+        return import_attribute(backend_config)()
+    elif isinstance(backend_config, dict):
+        # We have kwargs to be passed to the __init__ of the backend
+        backend_class = backend_config.get("CLASS")
+        if not backend_class:
+            raise ImproperlyConfigured("%s backend is misconfigured!" % alias)
+        return import_attribute(backend_class)(**backend_config)
+
+    raise ImproperlyConfigured("Django Sms engine's backend is misconfigured!")
 
 
 def get_available_backends():
-    backends = get_config().get('BACKENDS', {})
+    """ Get BACKENDS from sms_engine config
+        There must be at least one backend
+        and at least one of the backends is called `default` !
+        Empty backend should not be allowed
+    """
+    backends = get_config().get('BACKENDS')
 
-    if backends:
-        if not backends.get('default'):
-            raise ImproperlyConfigured('default backend is required')
-        return backends
-
-    backends['default'] = 'sms_engine.backends.TwilioBackend'
+    if not backends or not backends.get('default'):
+        raise ImproperlyConfigured('default backend is required')
 
     return backends
 
