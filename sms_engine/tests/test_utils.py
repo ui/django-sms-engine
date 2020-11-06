6b57492cd990ad109de032f86da8686a737e0b24
ahmadajip55 <pangestuajiahmad@gmail.com>
2020-11-06 15:08:05 +0700

    function sms_usage

---
+++ sms_engine/tests/test_utils.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+from sms_engine.sms import create as create_sms
+from sms_engine.utils import get_sms_usage
+
+
+class UtilsTest(TestCase):
+
+    def test_get_sms_usage(self) -> None:
+        create_sms(to="+62800000000001", message="Test", backend='dummy', scheduled_time="2020-11-06")
+        sms_usage = get_sms_usage("2020-11-01", "2020-11-15")
+        self.assertEqual(sms_usage["dummy"]["total"], 1)
--- sms_engine/utils.py
+++ sms_engine/utils.py
@@ -1,9 +1,11 @@
 from .compat import string_types
-from .models import PRIORITY
+from .models import PRIORITY, SMS
 from .settings import get_default_priority
 
+from typing import Any
 
-def parse_priority(priority):
+
+def parse_priority(priority: str) -> str:
     if priority is None:
         priority = get_default_priority()
     # If priority is given as a string, returns the enum representation
@@ -16,8 +18,32 @@ def parse_priority(priority):
     return priority
 
 
-def split_smss(smss, split_count=1):
+def split_smss(smss: list, split_count: int = 1) -> Any:
     # Group smss into X sublists
     # taken from http://www.garyrobinson.net/2008/04/splitting-a-pyt.html
     if list(smss):
         return [smss[i::split_count] for i in range(split_count)]
+
+
+def get_sms_usage(start_date: str, end_date: str) -> dict:
+    if start_date > end_date:
+        raise ValueError('`start` must be earlier than `end`')
+
+    smss = SMS.objects.filter(scheduled_time__range=[start_date, end_date])
+
+    result_sms_usage: dict = {}
+
+    for sms in smss:
+        if sms.backend_alias not in result_sms_usage:
+            if sms.status == 0:
+                result_sms_usage[sms.backend_alias] = {"total": 1, "succes": 1, "failed": 0}
+            else:
+                result_sms_usage[sms.backend_alias] = {"total": 1, "succes": 0, "failed": 1}
+        else:
+            result_sms_usage[sms.backend_alias]["total"] += 1
+            if sms.status == 0:
+                result_sms_usage[sms.backend_alias]["succes"] += 1
+            else:
+                result_sms_usage[sms.backend_alias]["failed"] += 1
+
+    return result_sms_usage
