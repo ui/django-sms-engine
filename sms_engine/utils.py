c3e03e8d1649859e03e38962873770032c9c16a1
Ibadarrohman <mkhashtag@gmail.com>
2020-11-13 15:23:26 +0700

    primitive functuon sms reports

--- sms_engine/utils.py
+++ sms_engine/utils.py
@@ -1,5 +1,10 @@
+import csv
+from datetime import date
+from io import StringIO
+from typing import Tuple
+
 from .compat import string_types
-from .models import PRIORITY
+from .models import PRIORITY, SMS
 from .settings import get_default_priority
 
 
@@ -21,3 +26,39 @@ def split_smss(smss, split_count=1):
     # taken from http://www.garyrobinson.net/2008/04/splitting-a-pyt.html
     if list(smss):
         return [smss[i::split_count] for i in range(split_count)]
+
+
+def get_monthly_csv_raw_data(start: date, end: date) -> StringIO:
+    smses = SMS.objects.filter(created__date__range=(start, end))
+    csv_buffer = StringIO()
+
+    csv_writer = csv.writer(csv_buffer)
+    csv_writer.writerow([
+        'Destination', 'Content', 'Description', 'Backend'
+    ])
+    for sms in smses:
+        csv_writer.writerow([
+            sms.to, sms.message, sms.description, sms.backend_alias
+        ])
+    return csv_buffer
+
+
+def parse_sms_raw_data_csv(file: StringIO, otp_descriptions: list) -> Tuple[StringIO, StringIO]:
+    # count otp and non otp sms
+    reader = csv.DictReader(file)
+
+    otp_csv_buffer = StringIO()
+    non_otp_csv_buffer = StringIO()
+
+    otp_sms_writer = csv.writer(otp_csv_buffer)
+    otp_sms_writer.writerow(reader.fieldnames)
+
+    non_otp_sms_writer = csv.writer(non_otp_csv_buffer)
+    non_otp_sms_writer.writerow(reader.fieldnames)
+
+    for row in reader:
+        if row['Description'] in otp_descriptions:
+            otp_sms_writer.writerow(row.values())
+        else:
+            non_otp_sms_writer.writerow(row.values())
+    return otp_csv_buffer, non_otp_csv_buffer
